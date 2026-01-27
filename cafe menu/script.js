9fa964765daaa1d0d199056389dfeb304dfb1781
kukoorup <yedukidhu@gmail.com>
2026-01-27 16:17:22 +0530

    Add files via upload

---
+++ cafe menu/script.js
@@ -0,0 +1,11 @@
+const sections = document.querySelectorAll(".fade");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+sections.forEach(sec => observer.observe(sec));
